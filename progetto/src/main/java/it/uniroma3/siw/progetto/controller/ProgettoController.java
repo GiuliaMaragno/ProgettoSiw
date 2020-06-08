3687c360962802b44ea12f3893f74242f00cb923
Alice D'Alberto <alicedalberto@MacBook-Pro-di-Alice.local>
2020-06-08 23:06:35 +0200

    una modifica estetica non rilevante

--- progetto/src/main/java/it/uniroma3/siw/progetto/controller/ProgettoController.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/controller/ProgettoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import it.uniroma3.siw.progetto.model.Credenziali;
 import it.uniroma3.siw.progetto.model.Progetto;
 import it.uniroma3.siw.progetto.model.Utente;
 import it.uniroma3.siw.progetto.repository.ProgettoRepository;
--- progetto/src/main/java/it/uniroma3/siw/progetto/service/UtenteService.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/service/UtenteService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import it.uniroma3.siw.progetto.model.Progetto;
 import it.uniroma3.siw.progetto.model.Utente;
 import it.uniroma3.siw.progetto.repository.UtenteRepository;
 
@@ -47,11 +46,7 @@ public class UtenteService {
 		return result;
 
 	}
-	
-	/*
-	 * @Transactional public List<Utente> getMembri(Progetto progetto){
-	 * 
-	 * }
-	 */
+
+
 
 }
